8fb0ce448fa7720d162b77643509fc4e41daa2fb
Raluca COANDA <ralucacoanda@air-de-raluca.home>
2026-06-27 15:22:52 +0200

    initialisation du projet

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-blue-600">
+        Hello Tailwind!
+      </h1>
+      <div className="bg-primary text-white p-4">
+        <h1 className="text-2xl font-bold">
+          Ceci utilise la couleur définie dans @theme
+        </h1>
+        <button className="border border-white hover:bg-white hover:text-primary">
+          Cliquez ici
+        </button>
+        <button className="btn-primary">
+          Mon Bouton
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
